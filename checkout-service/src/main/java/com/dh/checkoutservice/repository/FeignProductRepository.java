3445a469022586e8cc25954af4b81dbb1cad1145
EFFM00 <fischietto.elena@gmail.com>
2024-01-30 07:53:04 -0300

    consumo por url de un servicio con feign

--- checkout-service/src/main/java/com/dh/checkoutservice/repository/FeignProductRepository.java
+++ checkout-service/src/main/java/com/dh/checkoutservice/repository/FeignProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="product-service")
+@FeignClient(name="external-products", url="http://localhost:8089")
 public interface FeignProductRepository {
     @RequestMapping(method = RequestMethod.GET, value="/products")
     Product getProductById(@RequestParam String id);
