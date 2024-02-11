0027ce93841e9fbdcaa55ef5032848b1285e4a57
EFFM00 <fischietto.elena@gmail.com>
2024-02-11 16:37:19 -0300

    configuracion de notification service

--- payment-service/src/main/java/paymentservice/client/PaymentClient.java
+++ payment-service/src/main/java/paymentservice/client/PaymentClient.java
@@ -1,7 +1,9 @@
 package paymentservice.client;
 
+import org.springframework.stereotype.Service;
 import paymentservice.model.Payment;
 
+@Service
 public class PaymentClient {
     public Boolean createPayment(Payment payment) {
         // Ejecuta llamada a un servicio externo para ejecutar pago
--- payment-service/src/main/java/paymentservice/controller/PaymentController.java
+++ payment-service/src/main/java/paymentservice/controller/PaymentController.java
@@ -3,10 +3,12 @@ package paymentservice.controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import paymentservice.model.Payment;
 import paymentservice.service.PaymentService;
 
 @RequestMapping("/payments")
+@RestController
 public class PaymentController {
 
     private PaymentService paymentService;
