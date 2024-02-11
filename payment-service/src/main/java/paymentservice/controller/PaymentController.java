6c24d57ebedca6790bfbe6ba2c7459e6e7608b52
EFFM00 <fischietto.elena@gmail.com>
2024-02-11 15:11:29 -0300

    se agrega param para crear payment desde request body

--- payment-service/src/main/java/paymentservice/controller/PaymentController.java
+++ payment-service/src/main/java/paymentservice/controller/PaymentController.java
@@ -1,13 +1,23 @@
 package paymentservice.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import paymentservice.model.Payment;
+import paymentservice.service.PaymentService;
 
 @RequestMapping("/payments")
 public class PaymentController {
 
-    @PostMapping()
-    public void createPayment() {
+    private PaymentService paymentService;
 
+    public PaymentController(PaymentService paymentService) {
+        super();
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping()
+    public void createPayment(@RequestBody Payment payment) {
+        paymentService.createPayment((payment));
     }
 }
